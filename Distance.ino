ca93af6d03b45193d6bb897dd8278b7ed14a07c8
wang-dw <wangdw@miamioh.edu>
2017-03-13 15:51:28 -0400

    Arduino file

---
+++ Distance.ino
@@ -0,0 +1,75 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(11, 10, 9, 2, 3, 4, 5);
+
+const int trigPin = 12;
+const int echoPin = 13;
+
+void setup() {
+  lcd.begin(2, 12);
+  // initialize serial communication:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // establish variables for duration of the ping,
+  // and the distance result in inches and centimeters:
+  long duration, inches, cm;
+
+  // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  pinMode(trigPin, OUTPUT);
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // The same pin is used to read the signal from the PING))): a HIGH
+  // pulse whose duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.
+  pinMode(echoPin, INPUT);
+  duration = pulseIn(echoPin, HIGH);
+
+  // convert the time into a distance
+  inches = microsecondsToInches(duration);
+  cm = microsecondsToCentimeters(duration);
+
+  if(duration < 11600){
+    Serial.print(inches);
+    Serial.print("in, ");
+    Serial.print(cm);
+    Serial.print("cm");
+    Serial.println();
+
+    lcd.clear();
+    lcd.print("Inches: ");    
+    lcd.print(inches);
+    lcd.setCursor(0, 1); 
+    lcd.print("Centimeters: ");
+    lcd.print(cm); 
+  }
+  else{
+    Serial.print("Out of Range");
+    
+    lcd.clear();
+    lcd.print("Out of Range"); 
+  }
+  delay(200);
+}
+
+long microsecondsToInches(long microseconds) {
+  // According to Parallax's datasheet for the PING))), there are
+  // 73.746 microseconds per inch (i.e. sound travels at 1130 feet per
+  // second).  This gives the distance travelled by the ping, outbound
+  // and return, so we divide by 2 to get the distance of the obstacle.
+  // See: http://www.parallax.com/dl/docs/prod/acc/28015-PING-v1.3.pdf
+  return microseconds / 74 / 2;
+}
+
+long microsecondsToCentimeters(long microseconds) {
+  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
+  // The ping travels out and back, so to find the distance of the
+  // object we take half of the distance travelled.
+  return microseconds / 29 / 2;
+}
